143bc0e88c0896793a5aee1ae0d201c9491e5f14
Priyank Adhav <priyankadhav03@gmail.com>
2025-11-01 21:32:23 +0530

    feat: add support for additional icons and icon file normalization

--- image_generator.sh
+++ image_generator.sh
@@ -5,6 +5,7 @@ ICON_DIR="$THEME_DIR/os_icons"
 OUTPUT_DIR="$THEME_DIR"
 BASE_RED="$THEME_DIR/base_r.png"
 BASE_BLUE="$THEME_DIR/base_b.png"
+BASE_NONE="$THEME_DIR/base_n.png"
 
 if ! command -v magick >/dev/null 2>&1; then
     echo "ImageMagick (magick) not found! Please install it first."
@@ -72,14 +73,35 @@ fi
 
 TMP_DIR=$(mktemp -d)
 
-# Resize the icons
-magick "$ICON_DIR/os_antergos.png" -resize 120% "$TMP_DIR/$red_pill_icon"
-magick "$ICON_DIR/$blue_pill_icon" -resize 120% "$TMP_DIR/$blue_pill_icon"
-magick "$ICON_DIR/os_antergos.png" -resize 140% "$TMP_DIR/red_scaled.png"
-magick "$ICON_DIR/$blue_pill_icon" -resize 140% "$TMP_DIR/blue_scaled.png"
-magick "$ICON_DIR/func_shutdown.png" -resize 200% "$TMP_DIR/func_shutdown.png"
+# just for testing
+#red_pill_icon=os_linux.png
+#blue_pill_icon=os_linux.png
 
-# Recolor the icons
+for icon in "$ICON_DIR/$red_pill_icon" "$ICON_DIR/$blue_pill_icon" "$ICON_DIR/func_shutdown.png" "$ICON_DIR/func_firmware.png" "$ICON_DIR/func_hidden.png" "$ICON_DIR/selection.png"; do
+    filename=$(basename "$icon")
+    magick "$icon" \
+        -trim +repage \
+        -resize 130x130 \
+        -background none -gravity center \
+        -extent 312x312 "$TMP_DIR/$filename"
+done
+
+# Resize the icons
+# Adjust the percentage values to change the size of the icons
+# Base icons
+magick "$TMP_DIR/$red_pill_icon" -resize 120% "$TMP_DIR/$red_pill_icon"
+magick "$TMP_DIR/$blue_pill_icon" -resize 120% "$TMP_DIR/$blue_pill_icon"
+# Selected icons
+magick "$TMP_DIR/$red_pill_icon" -resize 120% "$TMP_DIR/red_scaled.png"
+magick "$TMP_DIR/$blue_pill_icon" -resize 120% "$TMP_DIR/blue_scaled.png"
+# Other icons
+magick "$TMP_DIR/func_shutdown.png" -resize 50% "$TMP_DIR/func_shutdown.png"
+magick "$TMP_DIR/func_hidden.png" -resize 50% "$TMP_DIR/func_hidden.png"
+magick "$TMP_DIR/func_firmware.png" -resize 50% "$TMP_DIR/func_firmware.png"
+# Selection background
+magick "$TMP_DIR/selection.png" -resize 150% "$TMP_DIR/selection.png"
+
+# Here you can change the colors of the icons
 magick "$TMP_DIR/$red_pill_icon" -fill "#FF0000" -colorize 100% "$TMP_DIR/$red_pill_icon"
 magick "$TMP_DIR/$blue_pill_icon" -fill "#00ebff" -colorize 100% "$TMP_DIR/$blue_pill_icon"
 magick  "$TMP_DIR/red_scaled.png" -fill "#FF0000" -colorize 100% "$TMP_DIR/red_scaled.png"
@@ -91,18 +113,59 @@ magick  "$TMP_DIR/func_shutdown.png" -fill "#f0f0f0" -colorize 100% "$TMP_DIR/fu
 magick "$BASE_BLUE" \
     "$TMP_DIR/blue_scaled.png" -geometry +1190+260 -composite \
     "$TMP_DIR/blue_scaled.png" -geometry +1190+260 -composite \
-    "$TMP_DIR/$red_pill_icon" -geometry +335+360 -composite \
-    "$TMP_DIR/$red_pill_icon" -geometry +335+360 -composite \
-    "$TMP_DIR/func_shutdown.png" -geometry +940+840 -composite \
+    "$TMP_DIR/$red_pill_icon" -geometry +335+345 -composite \
+    "$TMP_DIR/$red_pill_icon" -geometry +335+345 -composite \
+    "$TMP_DIR/func_firmware.png" -geometry +695+840 -composite \
+    "$TMP_DIR/func_shutdown.png" -geometry +895+840 -composite \
+    "$TMP_DIR/func_hidden.png" -geometry +1095+840 -composite \
     "$OUTPUT_DIR/selection_1.png"
 
 # Red pill selected
 magick "$BASE_RED" \
-    "$TMP_DIR/$blue_pill_icon" -geometry +1220+340 -composite \
-    "$TMP_DIR/$blue_pill_icon" -geometry +1220+340 -composite \
-    "$TMP_DIR/red_scaled.png" -geometry +305+245 -composite \
-    "$TMP_DIR/red_scaled.png" -geometry +305+245 -composite \
+    "$TMP_DIR/$blue_pill_icon" -geometry +1210+345 -composite \
+    "$TMP_DIR/$blue_pill_icon" -geometry +1210+345 -composite \
+    "$TMP_DIR/red_scaled.png" -geometry +305+260 -composite \
+    "$TMP_DIR/red_scaled.png" -geometry +305+260 -composite \
+    "$TMP_DIR/func_firmware.png" -geometry +695+840 -composite \
+    "$TMP_DIR/func_shutdown.png" -geometry +895+840 -composite \
+    "$TMP_DIR/func_hidden.png" -geometry +1095+840 -composite \
     "$OUTPUT_DIR/selection_0.png"
+
+# Firmware selected
+magick "$BASE_NONE" \
+    "$TMP_DIR/$blue_pill_icon" -geometry +1210+345 -composite \
+    "$TMP_DIR/$blue_pill_icon" -geometry +1210+345 -composite \
+    "$TMP_DIR/$red_pill_icon" -geometry +335+345 -composite \
+    "$TMP_DIR/$red_pill_icon" -geometry +335+345 -composite \
+    "$TMP_DIR/func_firmware.png" -geometry +695+840 -composite \
+    "$TMP_DIR/selection.png" -geometry +535+680 -composite \
+    "$TMP_DIR/func_shutdown.png" -geometry +895+840 -composite \
+    "$TMP_DIR/func_hidden.png" -geometry +1095+840 -composite \
+    "$OUTPUT_DIR/selection_2.png"
+
+# Shutdown selected
+magick "$BASE_NONE" \
+    "$TMP_DIR/$blue_pill_icon" -geometry +1210+345 -composite \
+    "$TMP_DIR/$blue_pill_icon" -geometry +1210+345 -composite \
+    "$TMP_DIR/$red_pill_icon" -geometry +335+345 -composite \
+    "$TMP_DIR/$red_pill_icon" -geometry +335+345 -composite \
+    "$TMP_DIR/func_firmware.png" -geometry +695+840 -composite \
+    "$TMP_DIR/func_shutdown.png" -geometry +895+840 -composite \
+    "$TMP_DIR/selection.png" -geometry +735+680 -composite \
+    "$TMP_DIR/func_hidden.png" -geometry +1095+840 -composite \
+    "$OUTPUT_DIR/selection_3.png"
+
+# Hidden selected
+magick "$BASE_NONE" \
+    "$TMP_DIR/$blue_pill_icon" -geometry +1210+345 -composite \
+    "$TMP_DIR/$blue_pill_icon" -geometry +1210+345 -composite \
+    "$TMP_DIR/$red_pill_icon" -geometry +335+345 -composite \
+    "$TMP_DIR/$red_pill_icon" -geometry +335+345 -composite \
+    "$TMP_DIR/func_firmware.png" -geometry +695+840 -composite \
+    "$TMP_DIR/func_shutdown.png" -geometry +895+840 -composite \
+    "$TMP_DIR/func_hidden.png" -geometry +1095+840 -composite \
+    "$TMP_DIR/selection.png" -geometry +935+680 -composite \
+    "$OUTPUT_DIR/selection_4.png"
 
 # Clean up the temporary dir
 rm -rf "$TMP_DIR"
